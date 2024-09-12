c5565480891348be06fa5c78d4d8b6268ee00320
Shreyas20004 <shreyas2004src@gmail.com>
2024-09-12 23:43:24 +0530

    Fixed an error

--- main.py
+++ main.py
@@ -64,6 +64,6 @@ def matcher():
 
 
 if __name__ == "__main__":
-    if not os.path.exists(app.config["UPLOADS_FOLDER"]):
-        os.makedirs(app.config['UPLOADS_FOLDER'])
+    if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(debug=True)
