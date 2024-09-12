32ebf065940c24f2c88eb08251aba21fa53b00c2
Shreyas20004 <shreyas2004src@gmail.com>
2024-09-12 23:39:07 +0530

    Project Completed :)

--- main.py
+++ main.py
@@ -1,14 +1,69 @@
+import os
 from flask import Flask, request,render_template
+import PyPDF2
+import docx2txt
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+#Helper Functions
+def extract_text_from_docx(file_path):
+    return docx2txt.process(file_path)
+def extract_text_from_txt(file_path):
+    with open(file_path,'r',encoding='utf-8') as file:
+        file.read()
+def extract_text_from_pdf(file_path):
+    text = ""
+    with open(file_path,"rb") as file:
+        reader = PyPDF2.PdfReader(file)
+        for page in reader.pages:
+            text += page.extract_text(page)
+        return text
+def extract_text(file_path):
+    if file_path.endswith('.pdf'):
+        return extract_text_from_pdf(file_path)
+    if file_path.endswith('.docx'):
+        return extract_text_from_docx(file_path)
+    if file_path.endswith('.txt'):
+        return extract_text_from_txt(file_path)
+    else: return ""
 @app.route("/")
 def matchResume():
     return render_template("matchResume.html")
 
 @app.route("/matcher",methods = ['GET','POST'])
 def matcher():
+    if request.method=="POST":
+        job_description = request.form.get('job_description')
+        resume_files = request.files.getlist('resumes')
+
+        resumes = []
+        for resume in resume_files:
+            filename = os.path.join(app.config['UPLOAD_FOLDER'],resume.filename)
+            resume.save(filename)
+            resumes.append(extract_text(resume))
+
+        if not resumes and not job_description:
+            return render_template('matchResume.html',message="Please upload resumes and post job....")
+
+        vectoriser=TfidfVectorizer.fit_transform([job_description]+resumes)
+        vectors = vectoriser.toarray()
+        job_vector = vectors[0]
+        resumes_vector = vectors[1:]
+        similarities = cosine_similarity([job_vector],resumes_vector)[0]
+
+        top_indices = similarities.argsort()[-3:][::-1]
+        top_resumes = [resume_files[i].filename for i in top_indices]
+        similarity_score = [round(similarities[i],2) for i in top_indices]
+
+        return render_template('matchResume.html', message="Top matchin resumes:",top_resumes=top_resumes,similarity_score=similarity_score)
+    return render_template('matchingResume.html')
+
 
 
 if __name__ == "__main__":
+    if not os.path.exists(app.config["UPLOADS_FOLDER"]):
+        os.makedirs(app.config['UPLOADS_FOLDER'])
     app.run(debug=True)
